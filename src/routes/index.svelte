30f83c6fb25b88d1958bbfcaf169f7146bc4563e
andrewbrandts <andrew.hagens@finaps.nl>
2022-05-05 09:32:29 -0400

    fix: changed section enumerations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,18 +3,20 @@
     import Legend from "$lib/features/legend/Legend.svelte";
     import input_data from "$lib/data/techRadarEntries.json";
 
+    let date = new Date();
+
     const config = {
         sections: [
             {
-                name: "LANGUAGES",
+                name: "LANGUAGES_AND_FRAMEWORKS",
                 label: "Languages & Frameworks",
                 index: 0,
                 color: "#fea000"
 
             },
             {
-                name: "FRAMEWORKS",
-                label: "Frameworks",
+                name: "PLATFORMS",
+                label: "Platforms",
                 index: 1,
                 color: "#191970"
             },
@@ -32,8 +34,8 @@
 
             },
             {
-                name: "METHODS",
-                label: "Methods",
+                name: "DATASTORES_DATAMANAGEMENT",
+                label: "Datastore & Data Management",
                 index: 4,
                 color: "#01bfad"
             }
@@ -43,30 +45,53 @@
                 index: 0,
                 name: "ADOPT",
                 label: "Adopt",
-                radius: 130
+                radius: 80
             },
             {
                 index: 1,
                 name: "TRAIL",
                 label: "Trail",
-                radius: 220
+                radius: 170
             },
             {
                 index: 2,
                 name: "ASSESS",
                 label: "Assess",
-                radius: 310
+                radius: 260
             },
             {
                 index: 3,
                 name: "HOLD",
                 label: "Hold",
-                radius: 400
+                radius: 350
             }
         ]
     };
 
     // pre-process input_data
+    // TODO Change this back to input file
+    // const input_data = [
+    //     {
+    //         "name": "Xamarin",
+    //         "section": "METHODS",
+    //         "status": "HOLD"
+    //     },
+    //     {
+    //         "name": "Test4",
+    //         "section": "METHODS",
+    //         "status": "TRAIL"
+    //     },
+    //     {
+    //         "name": "Xamarin",
+    //         "section": "METHODS",
+    //         "status": "HOLD"
+    //     },
+    //     {
+    //         "name": "Test4",
+    //         "section": "METHODS",
+    //         "status": "ASSESS"
+    //     },
+    // ];
     const entries = input_data.map((data, index) => {
         const slice = config.sections.find(sec => sec.name === data.section).index;
         const ring = config.rings.find(ring => ring.name === data.status).index;
@@ -83,11 +108,11 @@
     <title>Finaps Technology Radar</title>
 </svelte:head>
 
-<h1>Finaps Tech Radar 0.1</h1>
 
-<section class="sidebar">
+<h1 class="p-0">Finaps Tech Radar {date.getFullYear()}</h1>
+
+<section class="sidebar p-0">
     <Radar {entries} {config} />
 </section>
-
 
 <Legend {entries} {config} />
