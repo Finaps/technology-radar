e1698e87fce209124c9fb208ccbe89a46113c1fd
Jeroen Kesteloo <jeroen.kesteloo@finaps.nl>
2024-04-24 13:53:43 +0200

    Fixed issues after code review
    fix: removed unused #root style from app.css
    
    fix: added styling for img-circles
    
    fix: moved config constants (quadrants, rings and radar config) to a separate file
    
    fix: renamed config.json to data.json
    fix: renamed radar.css to app.css
    
    fix: removed custom styles for favicons

--- docs/app.js
+++ docs/app.js
@@ -1,40 +1,6 @@
-const quadrants = [
-  {
-    name: 'Techniques',
-    id: 4,
-    description:
-      'are elements of a software process, such as experience design and/or code, methodologies, and ways of structuring the software as microservices.',
-  },
-  {
-    name: 'Platforms',
-    id: 1,
-    description:
-      'similar to the previous one, they are software that helps in the development; however, they are made available without direct team maintenance. Overall, they fit in as a PaaS and/or SaaS.',
-  },
-  {
-    name: 'Languages & Frameworks',
-    id: 2,
-    description:
-      'are the tools that we use directly in the development of projects',
-  },
-  {
-    name: 'Tools',
-    id: 3,
-    description:
-      'we use software directly for development, requiring direct maintenance. For example, a database, a container orchestrator, etc.',
-  },
-];
-
-const rings = [
-  { name: 'ADOPT', color: 'var(--color-accent-1)', id: 0 },
-  { name: 'TRIAL', color: 'var(--color-accent-2)', id: 1 },
-  { name: 'ASSESS', color: 'var(--color-accent-3)', id: 2 },
-  { name: 'HOLD', color: 'var(--color-accent-4)', id: 3 },
-];
-
 let dataSet = [];
 
-fetch('./config.json')
+fetch('./data.json')
   .then(function (response) {
     return response.json();
   })
@@ -48,28 +14,12 @@ fetch('./config.json')
 
     document.getElementById('version').appendChild(span);
 
-    radar_visualization({
-      svg_id: 'radar',
-      width: 1400,
-      height: 1000,
-      colors: {
-        background: 'var(--bg-base-color)',
-        grid: '#dddde0',
-        inactive: '#ddd',
-        bubble: 'var(--font-color-inverse)',
-      },
-      title: 'Finaps Tech Radar',
+    const radarInput = {
+      ...RADAR_CONFIG,
       date: data.date,
-      quadrants: quadrants,
-      rings: rings,
-      print_rings: true,
-      print_title: false,
-      print_legend: true,
-      print_layout: false,
-      links_in_new_tabs: true,
-      // zoomed_quadrant: 0,
       entries: data.entries,
-    });
+    };
+    radar_visualization(radarInput);
 
     dataSet.forEach(function (item) {
       let label = document.createElement('button');
@@ -83,7 +33,6 @@ fetch('./config.json')
 
       let favicon = document.createElement('img');
       favicon.setAttribute('class', 'img-circle');
-      favicon.setAttribute('style', 'max-width: 1.5rem;');
 
       if (!item.link) {
         favicon.setAttribute(
@@ -123,13 +72,11 @@ function closeModal() {
   // document.getElementsById('tech-details-header').remove();
 }
 
-window.addEventListener('load', function () {
-  // close modals on background click
-  document.addEventListener('click', (event) => {
-    if (event.target.classList.contains('modal')) {
-      closeModal();
-    }
-  });
+// close modals on background click
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('modal')) {
+    closeModal();
+  }
 });
 
 function openTechDetails(index) {
@@ -139,8 +86,7 @@ function openTechDetails(index) {
 
   // favicon
   let favicon = document.createElement('img');
-  favicon.setAttribute('class', 'img-circle');
-  favicon.setAttribute('style', 'max-width: 2.5rem;');
+  favicon.setAttribute('class', 'img-circle img-lg');
   favicon.setAttribute(
     'src',
     `https://www.google.com/s2/favicons?domain=${item.link}&sz=128`,
